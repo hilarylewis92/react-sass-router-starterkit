08c3d79cd949ee16446290cd05ff7a523d09a376
Hilary Lewis <hilarylewis92@gmail.com>
2017-06-28 17:03:18 -0600

    Add browser router in index file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Home />
+  </div>
+)
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="Header">
+    <h2>Header component here </h2>
+  </div>
+)
+
+export default Header
