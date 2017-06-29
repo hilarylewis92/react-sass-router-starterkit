1a4e6701dcaff3c85aba6fed2000ea0cd55118bc
Hilary Lewis <hilarylewis92@gmail.com>
2017-06-29 10:46:42 -0600

    Add skeleton sass to index.js file'

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Home = () => (
   <div className="Home">
     <h2>Home Component </h2>
+    <form>
+      <input type='text'/>
+    </form>
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-import './styles/index.scss'
 require('skeleton-sass')
 
 ReactDOM.render((
